ac3e347696fe586831263bd4266c67b1c873bd8c
Alper Çırak <alprcrk@hotmail.com>
2024-02-17 20:22:04 +0300

    Platform bugfix

--- src/pages/Activity/OffCanvas/OffCanvasMenu.tsx
+++ src/pages/Activity/OffCanvas/OffCanvasMenu.tsx
@@ -23,7 +23,7 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 			show={show}
 			onHide={handleClose}
 			placement="end"
-			className="off-canvas-menu" // Güncellenmiş class ismi
+			className="off-canvas-menu" 
 		>
 			<Offcanvas.Header closeButton></Offcanvas.Header>
 			<Offcanvas.Body>
@@ -33,13 +33,13 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 						<div className="" style={{ width: " 160px" }}>
 							<img
 								src="https://lms.tobeto.com/tobeto/eep/common_show_picture_cached.aspx?pQS=eaAjNZ0uaOFNO7nf8wuXoA%3d%3d"
-								className="education-image" // Güncellenmiş class ismi
+								className="education-image" 
 								alt="Eğitim Resmi"
 							/>
 						</div>
 
 						<div className="col">
-							<div className=" col-8 education-title"> {/* Güncellenmiş class ismi */}
+							<div className=" col-8 education-title"> 
 								<div
 									className="row "
 									style={{
@@ -52,7 +52,7 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 								>
 									.NET & React Fullstack | Öğrenme Yolculuğu{" "}
 								</div>
-								<div className="row  education-detail-row"> {/* Güncellenmiş class ismi */}
+								<div className="row  education-detail-row"> 
 									<div
 										className="row "
 										style={{
@@ -63,7 +63,7 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 											width: "75px",
 										}}
 									>
-										<div className="video-detail-button">VİDEO</div> {/* Güncellenmiş class ismi */}
+										<div className="video-detail-button">VİDEO</div> 
 									</div>
 
 									<div
@@ -80,7 +80,7 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 										<FontAwesomeIcon icon={faClock} className="clock-icon" />
 									</div>
 									<div
-										className="row  time" // Güncellenmiş class ismi
+										className="row  time" 
 										style={{
 											paddingTop: "4px",
 											marginTop: "8px",
@@ -93,7 +93,7 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 										5dk
 									</div>
 									<div
-										className="row  time" // Güncellenmiş class ismi
+										className="row  time" 
 										style={{
 											paddingTop: "7px",
 											marginTop: "8px",
@@ -106,7 +106,7 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 										<FontAwesomeIcon icon={faEye} />
 									</div>
 									<div
-										className="row  time" // Güncellenmiş class ismi
+										className="row  time"
 										style={{
 											paddingTop: "3px",
 											marginTop: "8px",
@@ -123,9 +123,9 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 						</div>
 						<div className="col " style={{ marginTop: "40px" }}>
 							<div className="row  " style={{ marginBottom: "25px" }}>
-								<span className="go-education-button">EĞİTİME GİT </span> {/* Güncellenmiş class ismi */}
+								<span className="go-education-button">EĞİTİME GİT </span> 
 							</div>
-							<Row className="education-time-detail"> {/* Güncellenmiş class ismi */}
+							<Row className="education-time-detail">
 								<Col className="ok-icon">
 									{" "}
 									<FontAwesomeIcon icon={faThumbsUp} />
@@ -137,14 +137,14 @@ const OffcanvasMenu: React.FC<OffcanvasMenuProps> = ({ show, handleClose }) => {
 					</div>
 					<Row>
 						<Col className="">
-							<Row className="custom-education-detail-container"> {/* Güncellenmiş class ismi */}
+							<Row className="custom-education-detail-container"> 
 								<table>
 									<tr>
 										<td>
 											<FontAwesomeIcon icon={faList} className="icon" />
 										</td>
 										<td>Kategori</td>
-										<td className="custom-education-detail"> {/* Güncellenmiş class ismi */}
+										<td className="custom-education-detail"> 
 											İş'te Mükemmellik / Programlama / Programlama
 										</td>
 									</tr>

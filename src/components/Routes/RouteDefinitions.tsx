59f417054d39ad769c65a72bc814ab1048c6f8da
Tolga Kayış <tolgakayis_@hotmail.com>
2024-02-28 16:49:41 +0300

    formatting routes

--- src/components/Routes/RouteDefinitions.tsx
+++ src/components/Routes/RouteDefinitions.tsx
@@ -46,18 +46,117 @@ const RouteDefinitions: React.FC<Record<string, never>> = () => {
 	);
 	return (
 		<Routes>
-			<Route path="/" element={<><Banner /><CustomNavbar /><Homepage /><Footer /></>} />
-			<Route path="/giris" element={<><Banner /><CustomNavbar /><div className="login-center"><Login /></div><LoginFooter /></>} />
-			<Route path="/kayit-ol" element={<><Banner /><CustomNavbar /><div className="register-center"><Register /></div><LoginFooter /></>} />
-			<Route path="/platform" element={<><Navi /><PrivateRoute><Platform /></PrivateRoute><Footer /></>} />
-			<Route path="/egitimlerim" element={<><Navi /><PrivateRoute><TrainingSection /></PrivateRoute></>} />
-			<Route path="/duyurular" element={<><Navi /><PrivateRoute><AnnouncementSection /></PrivateRoute></>} />
-			<Route path="/sifremi-unuttum" element={<><Banner /><CustomNavbar /> <PasswordReset/><LoginFooter /></>}/>
-			<Route path="/istanbul-kodluyor" element={<><IstanbulKodluyor /></>} />
-			<Route path="/activity" element={<><PrivateRoute><Activity /></PrivateRoute></>} />
-			<Route path="/takvim" element={<><Navi /><PrivateRoute><Calendar/></PrivateRoute><Footer /></>}   />
-
-
+			<Route
+				path="/"
+				element={
+					<>
+						<Banner />
+						<CustomNavbar />
+						<Homepage />
+						<Footer />
+					</>
+				}
+			/>
+			<Route
+				path="/giris"
+				element={
+					<>
+						<Banner />
+						<CustomNavbar />
+						<div className="login-center">
+							<Login />
+						</div>
+						<LoginFooter />
+					</>
+				}
+			/>
+			<Route
+				path="/kayit-ol"
+				element={
+					<>
+						<Banner />
+						<CustomNavbar />
+						<div className="register-center">
+							<Register />
+						</div>
+						<LoginFooter />
+					</>
+				}
+			/>
+			<Route
+				path="/platform"
+				element={
+					<>
+						<Navi />
+						<PrivateRoute>
+							<Platform />
+						</PrivateRoute>
+						<Footer />
+					</>
+				}
+			/>
+			<Route
+				path="/egitimlerim"
+				element={
+					<>
+						<Navi />
+						<PrivateRoute>
+							<TrainingSection />
+						</PrivateRoute>
+					</>
+				}
+			/>
+			<Route
+				path="/duyurular"
+				element={
+					<>
+						<Navi />
+						<PrivateRoute>
+							<AnnouncementSection />
+						</PrivateRoute>
+					</>
+				}
+			/>
+			<Route
+				path="/sifremi-unuttum"
+				element={
+					<>
+						<Banner />
+						<CustomNavbar /> <PasswordReset />
+						<LoginFooter />
+					</>
+				}
+			/>
+			<Route
+				path="/istanbul-kodluyor"
+				element={
+					<>
+						<IstanbulKodluyor />
+					</>
+				}
+			/>
+			<Route
+				path="/activity"
+				element={
+					<>
+						<PrivateRoute>
+							<Activity />
+						</PrivateRoute>
+					</>
+				}
+			/>
+			<Route
+				path="/takvim"
+				element={
+					<>
+						<Navi />
+						<PrivateRoute>
+							<Calendar />
+						</PrivateRoute>
+						<Footer />
+					</>
+				}
+			/>
 
 			{/* profilim/profilimi-duzenle/profil-bilgilerim */}
 			<Route
